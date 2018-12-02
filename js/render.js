ebe937cd26dc7d7d97ef35960570f00c87813053
ttzztztz <935319340@qq.com>
2018-12-02 11:57:13 +0800

    data import

--- js/render.js
+++ js/render.js
@@ -23,43 +23,43 @@ const db_pre19c = [
 const db_19c = [
     {
         id: 1,
-        title: "hzytql",
-        year: "2016",
-        description: "",
+        title: "Reverie (1898)",
+        year: "Alphonse Mucha",
+        description: "Like Zodiac, this lithograph was originally designed to serve as the 1898 Champenois company calendar. However, its immediate popularity led to its swift publication by the magazine La Plume as a decorative panel with the title Reverie (daydream).Mucha's design shows a dreamy-eyed young woman leafing through a book of decorative designs, possibly printers' samples. The prominent disk behind her is elaborately decorated with flowers and their stems forming a lace-like pattern.",
         colors: ["#b54746", "#e6a98a", "#d68e76", "#b06256", "#dc2e25"],
     }, {
         id: 2,
-        title: "hzytql",
-        year: "2016",
-        description: "",
+        title: "Autographs Quilt (1856–ca.1863)",
+        year: "Adeline Harris Sears",
+        description: "In 1856, a seventeen-year-old girl from Rhode Island embarked on a unique and brilliant quiltmaking project. The girl’s name was Adeline Harris and her project was to make a quilt incorporating hundreds of celebrity autographs. While signature quilts were nothing new, the contributions were typically sourced from within a small community, such as a church, and functioned to commemorate a single event, such as a birth or marriage — Adeline, however, had bigger ideas, her community as the notable figures of her day, her event the phenomenon of nineteenth-century celebrity. Although one might imagine Adeline dutifully lugging a quilt to all corners of the globe for the famous to adorn with their scrawl, her process was much more ingenious (and practical). She sent a small diamond of white silk in the post with an explanation of her project and a request that they send it back to her signed. The returned and now autographed fragments were then worked into the quilt as the “top” planes in a wonderful trompe l’oeil tumbling block design. The response she got to her unusual request was nothing short of phenomenal — she ended up incorporating 360 signed pieces in total, including those from such luminaries as Jacob Grimm, Alexander von Humboldt, Charles Dickens, Ralph Waldo Emerson, and Abraham Lincoln (one of eight American presidents represented). According to her grand-daughter the Lincoln signature was, due to a family connection, actually acquired in person, and Adeline was meant to have even danced with Lincoln at his inauguration ball. Many of the pieces included a short message in addition to the signature. The diamond from the poet and editor N. P. Willis includesthe following (suggestive?) rhyme: “Dream what thou willst / beneath this quilt, / My blessing still is — Yours.”",
         colors: ["#e87769", "#efab96", "#c16c58", "#e7a686", "#a97277"],
     }, {
         id: 3,
-        title: "hzytql",
-        year: "2016",
-        description: "",
-        colors: ["#b7e68", "#f8decd", "#f89b8c", "#e1a79c", "#e1856c"],
+        title: "Chromolithographs of The Great Water Lily (1854)",
+        year: "William Sharp",
+        description: "These six magnificent colour lithographs are to be found in Victoria Regia, or, The Great Water Lily of America: With a Brief Account of its Discovery and Introduction into Cultivation (1854), a work by amateur botanist John Fisk Allen which documents his attempts to propagate the Amazon’s Victoria regia (now called Victoria amazonica) in the more northerly climes of his hometown of Salem, Massachusetts. The wonderfully lavish plates accompanying the slim volume are the work of the British-born printer William Sharp, who is credited with creating the very first chromolithograph on American soil — a portrait of Reverend F. W. P. Greenwood. These images produced for John Fisk Allen’s book are, according to Christies, the “very first colour-printed lithographs produced in America”. Why they are ignoring the Greenwood portrait, we are not quite sure: it could be that they mean a first in the context of large scale colour printing, as opposed to a one off; or perhaps it is a reflection that some believe it likely that the Greenwood portrait was actually a lithotint with colours printed from a single rather than the multiple stones normally associated with chromolithography. In any case, with their bold and stunning depth of colours, these water lily images by Sharp stand out as some of the finest examples of chromolithography, an art which at the time was only in its infancy.",
+        colors: ["#efac96", "#f8decd", "#f89b8c", "#e1a79c", "#e1856c"],
     }
 ];
 
 const db_20c = [
     {
         id: 1,
-        title: "hzytql",
-        year: "2017",
-        description: "",
+        title: "Space Colony Art (1970s)",
+        year: "Gerard O’Neill",
+        description: "In the 1970s the Princeton physicist Gerard O’Neill, with the help of NASA Ames Research Center and Stanford University, held a series of space colony summer studies which explored the possibilities of humans living in giant orbiting spaceships. Colonies housing about 10,000 people were designed and a number of artistic renderings of the concepts were made. ",
         colors: ["#7c5158", "#734144", "#a98e87", "#c9949a", "#cbaac9"],
     }, {
         id: 2,
-        title: "hzytql",
-        year: "2017",
-        description: "",
+        title: "Lord Tennyson’s Morte d’Arthur (1912)",
+        year: "Alberto Sangorski",
+        description: "Alfred Lord Tennyson’s Morte d’Arthur, written as early as the spring of 1835, was a retelling of the third, fourth and fifth chapters of the twenty-first book of Malory’s Romance about the legendary King Arthur, Guinevere, Lancelot, and the Knights of the Round Table. Tennyson later would incorporate a much extended and altered version of the poem into his The Idylls of the King, as the last section titled ‘The Passing of Arthur’. The illuminator Alberto Sangorski (1862-1932) was late to the world of calligraphy, at the age of 43 beginning to work for his younger brother Francis in the famous Sangorski and Sutcliffe bookbinding firm. One of his greatest achievements was a unique jewel bound version of Omar Khayyám’s Rubáiyát, now referred to as ‘the Great Omar,’ which never reached the American collector who commissioned it as it was sent across on the ill fated Titanic in 1912.",
         colors: ["#ea8d6c", "#f41b23", "#d48a71", "#d57569", "#c9867d"],
     }, {
         id: 3,
-        title: "hzytql",
-        year: "2017",
-        description: "",
+        title: "Jellyfish (1904)",
+        year: "Ernst Haeckel",
+        description: "The German biologist Ernst Haeckel was fascinated by medusae, the umbrella-shaped animals commonly called jellyfish. For Haeckel, whose imagination was shaped in the Romantic era, medusae expressed the exuberant yet fragile beauty of Nature. And in their ethereal forms he glimpsed a reflection of his great love Anna Sethe, who died tragically at the age of twenty-nine.<br> Haeckel had been engaged to Anna for four years when, in 1862, he became associate professor of zoology at the University of Jena. The job gave the adoring pair the economic security they needed to finally marry. In the same year, Haeckel published a book on radiolaria (microscopic plankton) which he furnished with stunning illustrations. In Jena, the newlyweds lived together in bliss for eighteen months. Then, on the day he was supposed to celebrate his thirtieth birthday and receive an award for his radiolaria book, Anna died suddenly, probably of a burst appendix. Haeckel became mad with grief. A partial delirium kept him in bed for eight days. A month later he wrote to a friend, “I am dead on the inside already and dead for everything. Life, nature, science have no appeal for me. How slowly the hours pass.”<br> Haeckel travelled to the Mediterranean town of Nice to attempt a recovery from his suicidal malaise. One day he took a walk and saw a medusa in a rock pool: “I enjoyed several happy hours watching the play of her tentacles which hang like blond hair-ornaments from the rim of the delicate umbrella-cap and which with the softest movement would roll up into thick short spirals.” He made a sketch and named the species Mitrocoma Annae [Anna’s headband].<br> The grace and beauty of the medusa soothed Haeckel’s grief and contributed to what would be a lifelong fascination with medusae. In The Tragic Sense of Life, Professor Robert J. Richard describes the profound impact of Anna’s death on him:<br> Through this acid mist, Haeckel resolved to devote himself single-mindedly to a cause that might transcend individual fragility. He would incessantly push the Darwinian ideal and oppose it to those who refused to look at life, to look at death, face on . . . After a period of recovery, Haeckel abandoned himself to an orgy of unrelenting work that yielded, after eighteen-hour days over twelve months, a mountainous two-volume monograph that laid out his fundamental ideas about evolution and morphology.<br> As part of his efforts to demonstrate that all living things are interconnected through evolution, he produced monographs on Siphonophorae (1869–88), Calcareous Sponges (1872), Arabian Corals (1876) and Medusae (1879–81). A year after completing the medusae book, a mighty two-volume work describing 600 species, Haeckel had a house built in Jena. He named it Villa Medusa and decorated the ceilings with frescoes of medusae that would later appear as lithographs in his classic book Art Forms in Nature (1899–1904). When one day a colleague showed him a new medusa species that he found even more beautiful than Mitrocoma Annae, he had to name it after his first wife too. He called it Desmonema annasetheand produced a sketch that the lithographer Adolf Giltsch would turn into arguably the most famous plate in Art Forms (see first image featured below).<br> If you would like to see more of Haeckel’s mesmerising jellyfish, he published a two-part monograph dedicated to them full of wonderful imagery: those in part one’s System of Medusae published in 1879 can be seen here, and those in part two, published in 1881, from reports of the British Challenger expedition (1873–76), can be seen here. Some decades later, in 1904, many medusae pictures (often based on Haeckel’s earlier monograph images) were turned into stunning lithographs by Adolf Giltsch and published in the immensely popular Art Forms of Nature (see Prestel’s 2008 edition here). If you’d like more on the life and work of Haeckel, in an essay for the Public Domain Review, Bernd Brunner explores the influence of a trip to Ceylon on Haeckel’s research as well as on his disturbing views on race and eugenics. And in another essay Dr Mario A. Di Gregorio explores Haeckel’s attempt to provide a bridge between biological science and art.",
         colors: ["#a6847a", "#dfc0ae", "#faa199", "#e28381", "#e05858"],
     }
 ];
@@ -67,21 +67,21 @@ const db_20c = [
 const db_21c = [
     {
         id: 1,
-        title: "hzytql",
-        year: "2018",
-        description: "",
+        title: "Gucci Bloom (2017)",
+        year: "Gucci",
+        description: "Debuting the first fragrance developed wholly under Alessandro Michele’s creative vision: a scent designed to celebrate the authenticity, vitality and diversity of women—flourishing in a natural, expressive and individual way. Blended by master perfumer Alberto Morillas under the direction of the House’s Creative Director, Gucci Bloom is created to unfold like its name, capturing the rich scent of a thriving garden filled with an abundance of flowers. Tuberose and jasmine combine with Rangoon Creeper—a unique flower discovered in South India that is being used for the first time in perfumery to create a rich fragrance that transports the wearer to an imaginary garden. The scent is presented in a lacquered bottle, reminiscent of porcelain, in a vintage powder pink shade with a Gucci label appliqué.",
         colors: ["#d6a6a2", "#f8988c", "#90312d", "#e9c4b1", "#aa603b"],
     },{
         id: 2,
-        title: "hzytql",
-        year: "2018",
-        description: "",
+        title: "Deserts of The Future (2018)",
+        year: "Al Mefer",
+        description: "The machinery of the future<br> Was thickening my breath<br> And well-predicted by computers<br> It would get us right to sleep<br> A dim light of strange hues <br> Opened my heavy eyes <br> With reflections of the moons<br> That colored alien skies<br> Of Western looks and colors<br> The deserts looked like dreams<br> And no winter, no summers<br> Would forge nor life nor seas<br>",
         colors: ["#ffd5cd", "#f26c6d", "#d78e99", "#fd8284", "#f5bec3"],
     },{
         id: 3,
-        title: "hzytql",
-        year: "2018",
-        description: "",
+        title: "JINJI KIKKO (2016)",
+        year: "王昱辰",
+        description: "All songs and lyrics written by Tseng, Kuo Hung <br> All songs arranged by Sunset Rollercoaster <br> Additional arrangements by Wang, Yuchain <br> Recorded by Wang, Yuchain at Rooftop Audio and Aii Studio, Taipei <br> Mixed by Wang, Yu-Chain at Rooftop Audio, Taipei <br> Mastered by Greg Calbi at Sterling Sound Studios, New York <br> Graphic Design by 林筱敏 Lin, Showme, 徐光慧 Sylvia Hsu, 蘇聖揚 Su Sheng Yang (Finger and Toe) <br> Sunset Rollercoaster comprised Tseng Kuo Hung, Chen Hung Li, Wang Shao Hsuan, Lo Tsun Lung, Chang Hao Chia<br>",
         colors: ["#d290b6", "#b53363", "#c95181", "#fc8be5", "#dd3280"],
     }
 ];
