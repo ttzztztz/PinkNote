1b37ccc7aab0d0462926c414941520e5cb6df18b
ttzztztz <935319340@qq.com>
2018-12-02 19:11:34 +0800

    list support

--- js/render.js
+++ js/render.js
@@ -17,6 +17,12 @@ const db_pre19c = [
         year: "Hieronymus Bosch",
         description: "在1490年至1510年间的某个时间，“人间花园”可能是Hieronymus Bosch最雄心勃勃的作品。由三个面板组成，总长度近四米，三联画是一个令人眼花缭乱的细节和令人愉快的超现实作品，其可能的意义使学者们忙碌了几个世纪。左侧小组 - 有时被称为亚当和夏娃的加入 - 是伊甸园中天堂般的场景的所在地，很可能是上帝将夏娃呈现给亚当的那一刻。虽然它在圣经故事中显然有其路线，但博世特别注意它 - 以其奇怪的野兽，亚当几乎是淫荡的目光 - 与西方艺术史上所描绘的任何其他东西不同。中间面板，三者中最大的，似乎从“第一”继续（三联画很可能是从左到右阅读），他们的天际线匹配。这些设置似乎也相互呼应，尽管中间面板的音调完全不同。在这里，场景是一个肆无忌惮的嬉戏，充满了裸体人物的景观，以及各种超大的动物，植物和巨大的水果 - 真实和幻想的混合。有这么多的嬉皮裸体，面板无疑是色情元素，但却带着一种纯真的感觉。根据博世学者威廉·弗兰格（Wilhelm Fraenger）的说法，这种色情主义含糊不清，既是精神过渡的寓言，也是腐败的游乐场。正确的面板与其他面板明显不同，显然是对地狱的描绘，这是博世作品的熟悉场景。他并没有退缩。这个场景尖叫着奇怪和噩梦般的图像：持刀的巨型耳朵，吃人的鸟怪，猪修女的不必要的进展，以及一个非凡的画面，一个巨大的粉红色怪物，唱着人体背面印有的乐谱。关闭左右面板揭示了这件作品的外部面板：几乎可以肯定的是对世界创造的描绘。在左上角可以看到上帝的小人物，在他的上方刻有诗篇33篇的引文 -  Ipse dixit，以及其他事实：ipsemandāvit，etcreatta sunt  - 因为他说话并且已经完成;他命令道，它站得很快。",
         colors: ["#89443d", "#987360", "#ad5e3f", "#942900", "#7c312e"],
+    }, {
+        id: 4,
+        title: "A General Map of the World (1794)",
+        year: "Samuel Dunn",
+        description: "A General Map of the World, or Terraqueouis Globe with all the New Discoveries and Marginal Delineations, Containing the Most Interesting Particulars in the Solar, Starry and Mundane System; an absolutely stunning and monumental double hemisphere wall map of the world by Samuel Dunn dating to 1794.",
+        colors: ["#e69ea2", "#d59d9e", "#e4c8c4", "#ba7578", "#ffb7cd"],
     }
 ];
 
@@ -39,6 +45,12 @@ const db_19c = [
         year: "William Sharp",
         description: "These six magnificent colour lithographs are to be found in Victoria Regia, or, The Great Water Lily of America: With a Brief Account of its Discovery and Introduction into Cultivation (1854), a work by amateur botanist John Fisk Allen which documents his attempts to propagate the Amazon’s Victoria regia (now called Victoria amazonica) in the more northerly climes of his hometown of Salem, Massachusetts. The wonderfully lavish plates accompanying the slim volume are the work of the British-born printer William Sharp, who is credited with creating the very first chromolithograph on American soil — a portrait of Reverend F. W. P. Greenwood. These images produced for John Fisk Allen’s book are, according to Christies, the “very first colour-printed lithographs produced in America”. Why they are ignoring the Greenwood portrait, we are not quite sure: it could be that they mean a first in the context of large scale colour printing, as opposed to a one off; or perhaps it is a reflection that some believe it likely that the Greenwood portrait was actually a lithotint with colours printed from a single rather than the multiple stones normally associated with chromolithography. In any case, with their bold and stunning depth of colours, these water lily images by Sharp stand out as some of the finest examples of chromolithography, an art which at the time was only in its infancy.",
         colors: ["#efac96", "#f8decd", "#f89b8c", "#e1a79c", "#e1856c"],
+    }, {
+        id: 4,
+        title: "The Great Barrier Reef of Australia (1893)",
+        year: "William Saville-Kent",
+        description: "While naming and arranging corals in the Natural History Department of the British Museum, William Saville-Kent daydreamed of seeing the beautiful grey organisms in front of him “in their native seas and wonderful living tints.” Years later he would realise his dream at the Great Barrier Reef in Queensland. There he conceived of a book that would be the first to extensively depict a coral reef in photographs — a selection of which are embedded at the bottom of this post.<br>Saville-Kent had grown frustrated by lack of promotion at the museum (where he worked under Richard Owen) and left in 1873 to become a resident naturalist at aquariums first in Brighton, then Manchester, and finally London. In 1884 he voyaged to the other side of the world and spent the next twenty years laying the foundations for fisheries development in Australia. He was particularly spellbound by the corals, polyps and other lifeforms on the Queensland coast. He hoped his book The Great Barrier Reef of Australia (1893) would increase public awareness of the reef and its origins, as well as encourage “development of its marvellous resources” — which was ominous when you consider its highly precarious predicament today.<br>The forty-eight photographs in the book were captured by Saville-Kent and then reproduced by the London Stereoscopic Company. The book’s biggest aesthetic delight, however, is perhaps reserved for the end. Here the reader would find sixteen beautiful colour lithographs created by a Mr Riddle and Mr Couchman from Saville-Kent’s original watercolour sketches. Though perhaps not the most accurate of depictions, something in the naive and almost cartoon-like representation seems to emphasise the vibrancy and life of the reef. (You can see a selection of the colour plates in our online prints shop here.)<br>The achievements of Saville-Kent never entirely shook a scandal which had followed him since he was fifteen. In 1860, his infant half-brother, Savill, was found murdered. His 16-year-old sister Constance was arrested but released without trial — public opinion distrusted the case of a working-class detective against a lady of breeding. Five years later, Constance confessed to the murder (though the Australian Dictionary of Biography calls it a “doubtful confession”) . Wilkie Collins put details of the case in The Moonstone (1868), the founding text of English detective fiction. As for William, there were suspicions that he had been an accomplice to his sister though he was never charged. His sister Constance escaped the gallows and died in 1944, at the age of one hundred, in Australia.\n",
+        colors: ["#b54746", "#e6a98a", "#d68e76", "#b06256", "#dc2e25"],
     }
 ];
 
@@ -61,6 +73,12 @@ const db_20c = [
         year: "Ernst Haeckel",
         description: "The German biologist Ernst Haeckel was fascinated by medusae, the umbrella-shaped animals commonly called jellyfish. For Haeckel, whose imagination was shaped in the Romantic era, medusae expressed the exuberant yet fragile beauty of Nature. And in their ethereal forms he glimpsed a reflection of his great love Anna Sethe, who died tragically at the age of twenty-nine.<br> Haeckel had been engaged to Anna for four years when, in 1862, he became associate professor of zoology at the University of Jena. The job gave the adoring pair the economic security they needed to finally marry. In the same year, Haeckel published a book on radiolaria (microscopic plankton) which he furnished with stunning illustrations. In Jena, the newlyweds lived together in bliss for eighteen months. Then, on the day he was supposed to celebrate his thirtieth birthday and receive an award for his radiolaria book, Anna died suddenly, probably of a burst appendix. Haeckel became mad with grief. A partial delirium kept him in bed for eight days. A month later he wrote to a friend, “I am dead on the inside already and dead for everything. Life, nature, science have no appeal for me. How slowly the hours pass.”<br> Haeckel travelled to the Mediterranean town of Nice to attempt a recovery from his suicidal malaise. One day he took a walk and saw a medusa in a rock pool: “I enjoyed several happy hours watching the play of her tentacles which hang like blond hair-ornaments from the rim of the delicate umbrella-cap and which with the softest movement would roll up into thick short spirals.” He made a sketch and named the species Mitrocoma Annae [Anna’s headband].<br> The grace and beauty of the medusa soothed Haeckel’s grief and contributed to what would be a lifelong fascination with medusae. In The Tragic Sense of Life, Professor Robert J. Richard describes the profound impact of Anna’s death on him:<br> Through this acid mist, Haeckel resolved to devote himself single-mindedly to a cause that might transcend individual fragility. He would incessantly push the Darwinian ideal and oppose it to those who refused to look at life, to look at death, face on . . . After a period of recovery, Haeckel abandoned himself to an orgy of unrelenting work that yielded, after eighteen-hour days over twelve months, a mountainous two-volume monograph that laid out his fundamental ideas about evolution and morphology.<br> As part of his efforts to demonstrate that all living things are interconnected through evolution, he produced monographs on Siphonophorae (1869–88), Calcareous Sponges (1872), Arabian Corals (1876) and Medusae (1879–81). A year after completing the medusae book, a mighty two-volume work describing 600 species, Haeckel had a house built in Jena. He named it Villa Medusa and decorated the ceilings with frescoes of medusae that would later appear as lithographs in his classic book Art Forms in Nature (1899–1904). When one day a colleague showed him a new medusa species that he found even more beautiful than Mitrocoma Annae, he had to name it after his first wife too. He called it Desmonema annasetheand produced a sketch that the lithographer Adolf Giltsch would turn into arguably the most famous plate in Art Forms (see first image featured below).<br> If you would like to see more of Haeckel’s mesmerising jellyfish, he published a two-part monograph dedicated to them full of wonderful imagery: those in part one’s System of Medusae published in 1879 can be seen here, and those in part two, published in 1881, from reports of the British Challenger expedition (1873–76), can be seen here. Some decades later, in 1904, many medusae pictures (often based on Haeckel’s earlier monograph images) were turned into stunning lithographs by Adolf Giltsch and published in the immensely popular Art Forms of Nature (see Prestel’s 2008 edition here). If you’d like more on the life and work of Haeckel, in an essay for the Public Domain Review, Bernd Brunner explores the influence of a trip to Ceylon on Haeckel’s research as well as on his disturbing views on race and eugenics. And in another essay Dr Mario A. Di Gregorio explores Haeckel’s attempt to provide a bridge between biological science and art.",
         colors: ["#a6847a", "#dfc0ae", "#faa199", "#e28381", "#e05858"],
+    }, {
+        id: 4,
+        title: "the Light of the Silvery Moon (1909)",
+        year: "Billy Murray & Haydon Quartet",
+        description: "Featured above is an early recording from 1910, by Billy Murray & Haydon Quartet, of this popular 20th-century Tin Pan Alley song made famous by Doris Day in the film of the same name released in 1953. The music was written by Gus Edwards and the lyrics by Edward Madden in 1909. Also featured below are versions by Ada Jones from 1912, Fats Waller from 1942, Al Jolson from 1949, and the Doris Day rendition from the musical film.",
+        colors: ["#ea8d6c", "#f41b23", "#d48a71", "#d57569", "#c9867d"],
     }
 ];
 
@@ -71,24 +89,34 @@ const db_21c = [
         year: "Gucci",
         description: "Debuting the first fragrance developed wholly under Alessandro Michele’s creative vision: a scent designed to celebrate the authenticity, vitality and diversity of women—flourishing in a natural, expressive and individual way. Blended by master perfumer Alberto Morillas under the direction of the House’s Creative Director, Gucci Bloom is created to unfold like its name, capturing the rich scent of a thriving garden filled with an abundance of flowers. Tuberose and jasmine combine with Rangoon Creeper—a unique flower discovered in South India that is being used for the first time in perfumery to create a rich fragrance that transports the wearer to an imaginary garden. The scent is presented in a lacquered bottle, reminiscent of porcelain, in a vintage powder pink shade with a Gucci label appliqué.",
         colors: ["#d6a6a2", "#f8988c", "#90312d", "#e9c4b1", "#aa603b"],
-    },{
+    }, {
         id: 2,
         title: "Deserts of The Future (2018)",
         year: "Al Mefer",
         description: "The machinery of the future<br> Was thickening my breath<br> And well-predicted by computers<br> It would get us right to sleep<br> A dim light of strange hues <br> Opened my heavy eyes <br> With reflections of the moons<br> That colored alien skies<br> Of Western looks and colors<br> The deserts looked like dreams<br> And no winter, no summers<br> Would forge nor life nor seas<br>",
         colors: ["#ffd5cd", "#f26c6d", "#d78e99", "#fd8284", "#f5bec3"],
-    },{
+    }, {
         id: 3,
         title: "JINJI KIKKO (2016)",
         year: "王昱辰",
-        description: "All songs and lyrics written by Tseng, Kuo Hung <br> All songs arranged by Sunset Rollercoaster <br> Additional arrangements by Wang, Yuchain <br> Recorded by Wang, Yuchain at Rooftop Audio and Aii Studio, Taipei <br> Mixed by Wang, Yu-Chain at Rooftop Audio, Taipei <br> Mastered by Greg Calbi at Sterling Sound Studios, New York <br> Graphic Design by 林筱敏 Lin, Showme, 徐光慧 Sylvia Hsu, 蘇聖揚 Su Sheng Yang (Finger and Toe) <br> Sunset Rollercoaster comprised Tseng Kuo Hung, Chen Hung Li, Wang Shao Hsuan, Lo Tsun Lung, Chang Hao Chia<br>",
+        description: "All songs and lyrics written by Tseng, Kuo Hung <br> All songs arranged by Sunset Rollercoaster <br> Additional arrangements by Wang, Yuchain <br> Recorded by Wang, Yuchain at Rooftop Audio and Aii Studio, Taipei <br> Mixed by Wang, Yu-Chain at Rooftop Audio, Taipei <br> Mastered by Greg Calbi at Sterling Sound Studios, New York <br> Graphic Design by 林筱敏 Lin, Showme, 徐光慧 Sylvia Hsu, 蘇聖揚 Su Sheng Yang (Finger and Toe) <br> Sunset Rollercoaster comprised Tseng Kuo Hung, Chen Hung Li, Wang Shao Hsuan, Lo Tsun Lung, Chang Hao Chia.",
         colors: ["#d290b6", "#b53363", "#c95181", "#fc8be5", "#dd3280"],
+    }, {
+        id: 4,
+        title: "REVISIÓN SONORA (2018)",
+        year: "Juan Bautista Espíndola",
+        description: "Un análisis de la escena cultural emergente entre 1980 y 1981: la música, el teatro, el cine, la fotografía, el fanzine... De David Bowie a Black Flag, pasando por Brian Eno, Robert Mapplethorpe, los Talking Heads Iggy Pop, Kraftwerk, DEVO o Chris Stein. La muerte de John Lennon, el devenir de los Sex Pistols, el fin de Joy Division y el nacimiento de New Order. El punk rock, la electrónica, el new wave. La fotografía, el cassette, la filmación digital, los productos, las modas y la publicidad de aquellos tiempos. El proyecto editorial fue realizado en el año 2017 para la asignatura \"Tipografía II\" de la cátedra Longinotti, carrera de Diseño Gráfico, FADU-UBA.",
+        colors: ["#eec1c4", "#e26c70", "#e98b8e", "#da9396", "#e77174"],
     }
 ];
 
 class Render {
     constructor() {
         this.db = [db_pre19c, db_19c, db_20c, db_21c];
+    }
+
+    list_find(index) {
+        return this.db[index];
     }
 
     db_find(index, id) {
